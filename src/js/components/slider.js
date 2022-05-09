965e65fa23c24ddf04d8b0e4e31bdf3b39204fc6
UserRF <you@example.com>
2022-05-09 15:58:18 +0300

    swiper

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,19 +1,22 @@
 const swiperSlider = (() => {
   const accountantInit = new Swiper('.swiper-main', {
-    slidesPerView: 2,
-    spaceBetween: 20,
+    slidesPerView: 1,
+    spaceBetween: 0,
     speed: 800,
+    loop: true,
     centerInsufficientSlides: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true,
-    },
-    breakpoints: {
-      768: {
-        spaceBetween: 40,
-        slidesPerView: 3,
-      },
     },
   });
 
